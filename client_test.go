e1b8146f293c24220afab85e55a012e0ae389dcb
Дмитрий Седых <dmitrys@xyzrd.com>
2016-09-24 19:56:27 +0300

    test

--- client_test.go
+++ client_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"sync"
 	"testing"
 	"time"
 )
@@ -80,32 +79,32 @@ func TestClient2(t *testing.T) {
 	}
 }
 
-func TestClientGoroutine(t *testing.T) {
-	certificate, err := LoadCertificate("cert.p12", "xopen123")
-	if err != nil {
-		t.Fatal("Load certificate error:", err)
-	}
-	client := New(*certificate)
-	tokens := []string{
-		"BE311B5BADA725B323B1A56E03ED25B4814D6B9EDF5B02D3D605840860FEBB28", // iPad
-		"507C1666D7ECA6C26F40BC322A35CCB937E2BF02DFDACA8FCCAAD5CEE580EE8C", // iPad mini
-		"6B0420FA3B631DF5C13FB9DDC1BE8131C52B4E02580BB5F76BFA32862F284572", // iPhone
-		// "6B0420FA3B631DF5C13FB9DDC1BE8131C52B4E02580BB5F76BFA32862F284570", // Bad
-	}
-	var wg sync.WaitGroup
-	wg.Add(len(tokens))
-	for _, token := range tokens {
-		go func(token string) {
-			id, err := client.Push(Notification{
-				Token:   token,
-				Payload: `{"aps":{"alert":"Test message"}}`,
-			})
-			fmt.Println(id)
-			wg.Done()
-			if err != nil {
-				fmt.Println("Push error:", err)
-			}
-		}(token)
-	}
-	wg.Wait()
-}
+// func TestClientGoroutine(t *testing.T) {
+// 	certificate, err := LoadCertificate("cert.p12", "xopen123")
+// 	if err != nil {
+// 		t.Fatal("Load certificate error:", err)
+// 	}
+// 	client := New(*certificate)
+// 	tokens := []string{
+// 		"BE311B5BADA725B323B1A56E03ED25B4814D6B9EDF5B02D3D605840860FEBB28", // iPad
+// 		"507C1666D7ECA6C26F40BC322A35CCB937E2BF02DFDACA8FCCAAD5CEE580EE8C", // iPad mini
+// 		"6B0420FA3B631DF5C13FB9DDC1BE8131C52B4E02580BB5F76BFA32862F284572", // iPhone
+// 		// "6B0420FA3B631DF5C13FB9DDC1BE8131C52B4E02580BB5F76BFA32862F284570", // Bad
+// 	}
+// 	var wg sync.WaitGroup
+// 	wg.Add(len(tokens))
+// 	for _, token := range tokens {
+// 		go func(token string) {
+// 			id, err := client.Push(Notification{
+// 				Token:   token,
+// 				Payload: `{"aps":{"alert":"Test message"}}`,
+// 			})
+// 			fmt.Println(id)
+// 			wg.Done()
+// 			if err != nil {
+// 				fmt.Println("Push error:", err)
+// 			}
+// 		}(token)
+// 	}
+// 	wg.Wait()
+// }
